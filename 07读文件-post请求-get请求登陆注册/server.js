c0406f3ce68bccdc5874d05c29288cbceb6ffad0
wang1984 <277920119@qq.com>
2017-08-25 09:32:00 +0800

    node常用接口第一部分

---
+++ 07读文件-post请求-get请求登陆注册/server.js
@@ -0,0 +1,86 @@
+/*
+用户注册、登录
+
+接口：
+/user?act=reg&user=aaa&pass=123456
+  {"ok": false, "msg": "原因"}
+
+/user?act=login&user=aaa&pass=123456
+  {"ok": true, "msg": "原因"}
+
+----------------------------------------------------------------------
+
+对文件访问：
+http://localhost:8080/1.html
+http://localhost:8080/ajax.js
+http://localhost:8080/1.jpg
+
+对接口访问：
+http://localhost:8080/user?act=xx...
+http://localhost:8080/news?act=xxx...
+*/
+
+const http=require('http');
+const fs=require('fs');
+const querystring=require('querystring');
+const urlLib=require('url');
+
+var users={};   //{"blue": "123456", "zhangsan": "123456", "lisi": "321321"}
+
+var server=http.createServer(function (req, res){
+  //解析数据
+  var str='';
+  req.on('data', function (data){
+    str+=data;
+  });
+  req.on('end', function (){
+    var obj=urlLib.parse(req.url, true);
+    /*url GET POST 都放在 post接收数据中是可以的*/ 
+    const url=obj.pathname;
+    const GET=obj.query;
+    const POST=querystring.parse(str);
+
+    //区分——接口、文件
+    if(url=='/user'){   //接口
+      switch(GET.act){
+        case 'reg':
+          //1.检查用户名是否已经有了
+          if(users[GET.user]){
+            res.write('{"ok": false, "msg": "此用户已存在"}');
+          }else{
+            //2.插入users
+            users[GET.user]=GET.pass;
+            res.write('{"ok": true, "msg": "注册成功"}');
+          }
+          break;
+        case 'login':
+          //1.检查用户是否存在
+          if(users[GET.user]==null){
+            res.write('{"ok": false, "msg": "此用户不存在"}');
+          //2.检查用户密码
+          }else if(users[GET.user]!=GET.pass){
+            res.write('{"ok": false, "msg": "用户名或密码有误"}');
+          }else{
+            res.write('{"ok": true, "msg": "登录成功"}');
+          }
+          break;
+        default:
+          res.write('{"ok": false, "msg": "未知的act"}');
+      }
+      res.end();
+    }else{              //文件
+      //读取文件
+      var file_name='./www'+url;
+      fs.readFile(file_name, function (err, data){
+        if(err){
+          res.write('404');
+        }else{
+          res.write(data);
+        }
+        res.end();
+      });
+    }
+  });
+});
+
+server.listen(8080);
